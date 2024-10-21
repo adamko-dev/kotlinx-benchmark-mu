e5e6013f38104bbe9e1dd68751af867e8db2078f
Adam <897017+aSemy@users.noreply.github.com>
2024-10-21 16:50:34 +0200

    use kotlin math fns

--- kxb-examples/kotlin-multiplatform/src/jvmBenchmark/kotlin/JvmBenchmark.kt
+++ kxb-examples/kotlin-multiplatform/src/jvmBenchmark/kotlin/JvmBenchmark.kt
@@ -2,6 +2,8 @@ package test
 
 import org.openjdk.jmh.annotations.*
 import java.util.concurrent.*
+import kotlin.math.cos
+import kotlin.math.sqrt
 
 @State(Scope.Benchmark)
 @Fork(1)
@@ -20,12 +22,11 @@ class JvmBenchmark {
 
     @Benchmark
     fun sqrtBenchmark(): Double {
-        return Math.sqrt(data)
+        return sqrt(data)
     }
 
     @Benchmark
     fun cosBenchmark(): Double {
-        return Math.cos(data)
+        return cos(data)
     }
 }
-
